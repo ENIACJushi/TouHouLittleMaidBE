2d0dd1ef30ca8f22228d5bb7a8d9567fb9962fe1
ENIACJushi <368364534@qq.com>
2024-06-05 17:40:09 +0800

    文件同步脚本

--- .Tools/copy.bat
+++ .Tools/copy.bat
@@ -1,1 +1,7 @@
-DEL "C:\Users\Dell\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\TouHouLittleMaid_BP"
+@echo off
+
+rmdir /s /q "C:\Users\Dell\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\TouHouLittleMaid_BP"
+xcopy /I /Q /s /e "D:\GitHub\TouHouLittleMaidBE\TouHouLittleMaid_BP" "C:\Users\Dell\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\TouHouLittleMaid_BP"
+
+rmdir /s /q "C:\Users\Dell\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_RP"
+xcopy /I /Q /s /e "D:\GitHub\TouHouLittleMaidBE\TouHouLittleMaid_RP" "C:\Users\Dell\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_RP"
