7a8f30c91a8f40c4ec138df981465d6d621c5cc2
ENIACJushi <1004210112@qq.com>
2025-05-18 18:33:42 +0800

    huh

--- .Tools/copy.bat
+++ .Tools/copy.bat
@@ -1,7 +1,12 @@
 @echo off
+@REM MinecraftPath: C:\Users\XXX\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe
 
-rmdir /s /q "C:\Users\Dell\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\TouHouLittleMaid_BP"
-xcopy /I /Q /s /e "D:\GitHub\TouHouLittleMaidBE\TouHouLittleMaid_BP" "C:\Users\Dell\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\TouHouLittleMaid_BP"
+@REM rmdir /s /q "%MinecraftPath%\LocalState\games\com.mojang\development_behavior_packs\TouHouLittleMaid_BP"
+@REM xcopy /I /Q /s /e "..\TouHouLittleMaid_BP" "%MinecraftPath%\LocalState\games\com.mojang\development_behavior_packs\TouHouLittleMaid_BP"
+@REM
+rmdir /s /q "%MinecraftPath%\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_RP"
+xcopy /I /Q /s /e "..\TouHouLittleMaid_RP" "%MinecraftPath%\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_RP"
 
-rmdir /s /q "C:\Users\Dell\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_RP"
-xcopy /I /Q /s /e "D:\GitHub\TouHouLittleMaidBE\TouHouLittleMaid_RP" "C:\Users\Dell\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_RP"
+rmdir /s /q "%MinecraftPath%\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_PBR"
+xcopy /I /Q /s /e "..\TouHouLittleMaid_PBR" "%MinecraftPath%\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_PBR"
+
