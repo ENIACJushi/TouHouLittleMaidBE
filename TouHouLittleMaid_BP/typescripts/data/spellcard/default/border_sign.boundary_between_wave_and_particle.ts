f5d113b2807ccf76ba024dbb8b352dd0641ebced
ENIACJushi <1004210112@qq.com>
2025-05-23 00:23:38 +0800

    波与粒增加发射次数配置项，便于演示

--- TouHouLittleMaid_BP/typescripts/data/spellcard/default/border_sign.boundary_between_wave_and_particle.ts
+++ TouHouLittleMaid_BP/typescripts/data/spellcard/default/border_sign.boundary_between_wave_and_particle.ts
@@ -14,23 +14,21 @@ import { EntityDanmakuActor } from "../../../src/danmaku/actors/EntityDanmakuAct
 export const SpellCard = {
   // 释放该符卡的物品id，必须以thlms为开头
   id: "thlms:border_sign.boundary_between_wave_and_particle",
-  // 作者，字符串
-  author: "tartaric_acid",
-  // 版本，字符串
-  version: "1.0.0",
-  // 冷却时间，整型数
-  cooldown: 250,
+  author: "tartaric_acid", // 作者，字符串
+  version: "1.0.0", // 版本，字符串
+  cooldown: 250, // 冷却时间，整型数
+  times: 120, // 发射次数 默认为 120
+
   /**
    * 执行的符卡逻辑，函数签名固定，会直接调用
-   * @param {Dimension} world 当前所处的世界
-   * @param {Entity} shooter 释放符卡的实体
+   * @param world 当前所处的维度
+   * @param shooter 释放符卡的实体
    */
   spellCard: function (world: Dimension, shooter: Entity) {
-    for (var i = 0; i < 120; i++) {
-      // Task.add 改为 system.runTimeout, 传值需要使用另外的方法
+    for (let i = 0; i < this.times; i++) {
       // 传值给延时命令
-      var shoot_basic = function ($times: number) {
-        for (var j = 0; j < 9; j++) {
+      let shoot_basic = function ($times: number) {
+        for (let j = 0; j < 9; j++) {
           // 原初始化参数：世界 实体, damage, gravity, type, color，除了前两个均要另外设置
           let bulletShoot = new BulletShoot({
             thrower: new EntityDanmakuActor(shooter)
@@ -39,9 +37,8 @@ export const SpellCard = {
               .setGeneralBulletType(Type.PETAL)
               .setColor(Color.MAGENTA)
               .setDamage(2)
-          })
+          });
 
-          // 原发射参数 entityThrower, rotationPitchIn, rotationYawIn, pitchOffset, velocity, inaccuracy
           // 旋转角设置
           let direction = VectorMC.rotate_axis(
             new Vector(0, 0, 1), 
@@ -51,7 +48,7 @@ export const SpellCard = {
           bulletShoot.shootByDirection(direction, 0.4, 0);
         }
       };
-      var shoot = function ($times: number) {
+      let shoot = function ($times: number) {
         return function () {
           shoot_basic($times);
         }
