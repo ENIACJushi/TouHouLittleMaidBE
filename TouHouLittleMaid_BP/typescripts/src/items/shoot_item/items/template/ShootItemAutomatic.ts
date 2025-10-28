1486177a18f8bf552d009d9f8b2ca12b4dd1fdc1
ENIACJushi <1004210112@qq.com>
2025-10-29 01:02:09 +0800

    优化射击函数

--- TouHouLittleMaid_BP/typescripts/src/items/shoot_item/items/template/ShootItemAutomatic.ts
+++ TouHouLittleMaid_BP/typescripts/src/items/shoot_item/items/template/ShootItemAutomatic.ts
@@ -44,8 +44,31 @@ export abstract class ShootItemAutomatic extends ShootItemBase {
   public startUseEvent (event: ItemStartUseAfterEvent) {
     let playerId = event.source.id;
     let slot = event.source.selectedSlotIndex;
+    // 确定发射间隔
+    let timeout = this.getCooldown(event.source, event.itemStack);
+    // 根据上次射击的时间，确定第一次发射的延迟，若此前足够长的时间没有发射过，则立即发射
+    let firstTimeout = Math.max(0, timeout - system.currentTick + (this.lastShootingTick.get(playerId) ?? 0));
+    let intervalId: number = -1;
     // 射击函数
-    let shoot = (player: Player, item: ItemStack) => {
+    let shoot = () => {
+      ///// 停止判断 /////
+      // 事件取消
+      if (intervalId !== this.getPlayerInterval(playerId)) {
+        system.clearRun(intervalId);
+        return false;
+      }
+      // 玩家消失 || 物品栏切换
+      let player = world.getEntity(playerId) as Player | undefined;
+      if (!player || player.getComponent('health')?.currentValue === 0 || slot !== player.selectedSlotIndex) {
+        this.clearPlayerInterval(playerId);
+        return false;
+      }
+      // 物品消失 || 物品不是御币
+      let item = ItemTool.getPlayerMainHand(player);
+      if (!item || !this.isShootItem(item)) {
+        this.clearPlayerInterval(playerId);
+        return false;
+      }
       // 发射弹幕
       this.shoot({ entity: player, item: item });
       this.lastShootingTick.set(playerId, system.currentTick);
@@ -53,56 +76,17 @@ export abstract class ShootItemAutomatic extends ShootItemBase {
       this.damageItem(item, player, slot);
       // 播放音效
       this.playShotSound(player);
+      return true;
     }
-    // 确定发射间隔
-    let timeout = this.getCooldown(event.source, event.itemStack);
-    // 根据上次射击的时间，确定第一次发射的延迟，若此前足够长的时间没有发射过，则立即发射
-    let firstTimeout = Math.max(0, timeout - system.currentTick + (this.lastShootingTick.get(playerId) ?? 0));
-    let intervalId = system.runTimeout(() => {
-      ///// 停止判断 /////
-      // 事件取消
-      if (intervalId !== this.getPlayerInterval(playerId)) {
-        system.clearRun(intervalId);
+    // 开始任务
+    intervalId = system.runTimeout(() => {
+      let res = shoot();
+      if (!res) {
         return;
       }
-      // 玩家消失 || 物品栏切换
-      let player = world.getEntity(playerId) as Player | undefined;
-      if (!player || slot !== player.selectedSlotIndex) {
-        this.clearPlayerInterval(playerId);
-        return;
-      }
-      // 物品消失 || 物品不是御币
-      let item = ItemTool.getPlayerMainHand(player);
-      if (!item || !this.isShootItem(item)) {
-        this.clearPlayerInterval(playerId);
-        return;
-      }
-      // 射击
-      shoot(player, item);
       // 开始后续的自动射击
       // 创建一个定时循环的任务，在玩家保持蓄力状态时执行，蓄力结束或满足其他结束条件时停止
-      intervalId = system.runInterval(() => {
-        ///// 停止判断 /////
-        // 事件取消
-        if (intervalId !== this.getPlayerInterval(playerId)) {
-          system.clearRun(intervalId);
-          return;
-        }
-        // 玩家消失 || 物品栏切换
-        let player = world.getEntity(playerId) as Player | undefined;
-        if (!player || slot !== player.selectedSlotIndex) {
-          this.clearPlayerInterval(playerId);
-          return;
-        }
-        // 物品消失 || 物品不是御币
-        let item = ItemTool.getPlayerMainHand(player);
-        if (!item || !this.isShootItem(item)) {
-          this.clearPlayerInterval(playerId);
-          return;
-        }
-        // 射击
-        shoot(player, item);
-      }, timeout);
+      intervalId = system.runInterval(shoot, timeout);
       this.registerPlayerInterval(playerId, intervalId);
     }, firstTimeout);
     this.registerPlayerInterval(playerId, intervalId);
