397eefbedb40444ef6d1baca075a70dab767423b
ENIACJushi <1004210112@qq.com>
2025-10-31 00:38:32 +0800

    更新新版本的模组目标路径

--- TouHouLittleMaid_BP/typescripts/cp.bat
+++ TouHouLittleMaid_BP/typescripts/cp.bat
@@ -1,12 +1,12 @@
 @echo off
-@REM MinecraftPath: C:\Users\XXX\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe
+@REM MinecraftPath: C:\Users\xxx\AppData\Roaming\Minecraft Bedrock\Users\Shared\games\com.mojang
 
-rmdir /s /q "%MinecraftPath%\LocalState\games\com.mojang\development_behavior_packs\TouHouLittleMaid_BP"
-xcopy /I /Q /s /e "..\..\TouHouLittleMaid_BP" "%MinecraftPath%\LocalState\games\com.mojang\development_behavior_packs\TouHouLittleMaid_BP"
+rmdir /s /q "%MinecraftPath%\development_behavior_packs\TouHouLittleMaid_BP"
+xcopy /I /Q /s /e "..\..\TouHouLittleMaid_BP" "%MinecraftPath%\development_behavior_packs\TouHouLittleMaid_BP"
 
-rmdir /s /q "%MinecraftPath%\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_RP"
-xcopy /I /Q /s /e "..\..\TouHouLittleMaid_RP" "%MinecraftPath%\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_RP"
+rmdir /s /q "%MinecraftPath%\development_resource_packs\TouHouLittleMaid_RP"
+xcopy /I /Q /s /e "..\..\TouHouLittleMaid_RP" "%MinecraftPath%\development_resource_packs\TouHouLittleMaid_RP"
 
-@REM rmdir /s /q "%MinecraftPath%\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_PBR"
-@REM xcopy /I /Q /s /e "..\..\TouHouLittleMaid_PBR" "%MinecraftPath%\LocalState\games\com.mojang\development_resource_packs\TouHouLittleMaid_PBR"
+@REM rmdir /s /q "%MinecraftPath%\development_resource_packs\TouHouLittleMaid_PBR"
+@REM xcopy /I /Q /s /e "..\..\TouHouLittleMaid_PBR" "%MinecraftPath%\development_resource_packs\TouHouLittleMaid_PBR"
 
