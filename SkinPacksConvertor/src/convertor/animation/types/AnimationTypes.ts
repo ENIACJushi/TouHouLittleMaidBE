0e8afd4c3a7f5157cb63662a9782f9c19b191195
ENIACJushi <1004210112@qq.com>
2026-08-02 23:05:35 +0800

    解析 pre_parallel0~7

--- SkinPacksConvertor/src/convertor/animation/types/AnimationTypes.ts
+++ SkinPacksConvertor/src/convertor/animation/types/AnimationTypes.ts
@@ -1,6 +1,12 @@
 
 /**
  * java 动画键名列表
+ *
+ * 添加一个动画解析的流程：
+ * - 在 `AnimationTemplates.ts` - `ANIMATION_DEF_TEMPLATE` 的
+ *    `scripts.pre_animation`、`scripts.animate`、`animations` 三处补充定义
+ * - 在 `AnimationTypes` 补充枚举
+ * - 在 `ANIMATE_EXTRA_CONDITION` 定义额外条件
  */
 export enum AnimationTypes {
   walk = 'walk',
@@ -10,6 +16,14 @@ export enum AnimationTypes {
   parallel1 = 'parallel1',
   parallel2 = 'parallel2',
   parallel3 = 'parallel3',
+  pre_parallel0 = 'pre_parallel0',
+  pre_parallel1 = 'pre_parallel1',
+  pre_parallel2 = 'pre_parallel2',
+  pre_parallel3 = 'pre_parallel3',
+  pre_parallel4 = 'pre_parallel4',
+  pre_parallel5 = 'pre_parallel5',
+  pre_parallel6 = 'pre_parallel6',
+  pre_parallel7 = 'pre_parallel7',
 
   // 可实现对应状态，但暂未加入
   // swing_hand = 'swing_hand',
@@ -21,10 +35,6 @@ export enum AnimationTypes {
   // swim_stand = 'swim_stand',
   // use_offhand = 'use_offhand',
   // use_mainhand = 'use_mainhand',
-  // pre_parallel0 = 'pre_parallel0',
-  // pre_parallel1 = 'pre_parallel1',
-  // pre_parallel2 = 'pre_parallel2',
-  // pre_parallel3 = 'pre_parallel3',
 
   // 目前没有对应功能的动画
   // gomoku = 'gomoku',
